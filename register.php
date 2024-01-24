06fdd583e69303d15c546d56a711799ac2bb1f9e
lyhryn <75486879+lyhryn@users.noreply.github.com>
2024-01-24 14:03:45 +0100

    register.php Design und eine Template PHP datei erstellt.

---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Knjižara - Registrieren</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="p-5 h-100 d-flex align-items-center justify-content-center">
+        <div class="p-3 border">
+            <div class="h-100 d-flex align-items-center justify-content-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                </svg>
+            </div>
+            <div class="h-100 d-flex align-items-center justify-content-center">
+                <p>Registrieren</p>
+            </div>
+            <form action="/action_page.php">
+                <div class="mb-3 mt-3">
+                    <label for="email" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Email eingeben" name="email">
+                </div>
+                <div class="mb-3">
+                    <label for="pwd" class="form-label">Passwort:</label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Passwort eingeben" name="pswd">
+                </div>
+                <div class="mb-3">
+                    <label for="pwd" class="form-label">Passwort wiederholen:</label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Passwort wiederholen" name="pswd">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
