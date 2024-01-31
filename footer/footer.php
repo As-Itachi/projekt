229f5643a526f1ece0dc748622045b34492489d6
lyhryn <75486879+lyhryn@users.noreply.github.com>
2024-01-31 16:44:14 +0100

    footer verschönert

--- footer/footer.php
+++ footer/footer.php
@@ -20,9 +20,10 @@
     <footer class="text-center text-lg-start bg-body-tertiary text-muted">
         <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
             <!-- Left -->
+            <!--
             <div class="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-            </div>
+                <span>Get connected with us on social networks:</span>
+            </div> -->
             
             <div>
             <a href="" class="me-4 text-reset">
