674b2b2bd997da34f0ea5e97da6b13a98d02ec19
Danijel Ritan <danijelritan6@gmail.com>
2024-02-02 16:15:18 +0100

    gerüst für das banner

--- hauptseite.php
+++ hauptseite.php
@@ -18,25 +18,65 @@
 
 <body data-bs-spy="scroll" data-bs-target="#header" tabindex="0">
 
-    <header id="header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-10">
-                     <nav id="navbar">
-                        <div class="main-menu stellarnav">
-                            <div class="hamburger">
-                                <span class="bar"></span>
-                                <span class="bar"></span>
-                                <span class="bar"></span>
+        <header id="header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-10">
+                        <nav id="navbar">
+                            <div class="main-menu stellarnav">
+                                <div class="hamburger">
+                                    <span class="bar"></span>
+                                    <span class="bar"></span>
+                                    <span class="bar"></span>
+                                </div>
                             </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+    <section id="billborad">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                <button class="prev slick-arrow">
+						<i class="icon icon-arrow-left"></i>
+				</button>
+
+                <div class="main-slider pattern-overlay">
+                    <div class="slider-item">
+                        <div class="banner-content">
+                            <h2 class="banner-title">The Ruby Circle</h2>
+                            <p>
+                            Als Louisa eines der seltenen Stipendien für die Highclare Academy erhält, erhofft sie sich einen Neuanfang. Nicht nur, weil sie als Studentin automatisch zum elitären Kreis des Ruby Circles zählt und ihr jeder Wunsch von den Augen abgelesen wird. Sondern auch, weil sie glaubt, dort endlich sicher vor den Lügen zu sein, die ihr Leben in einen Albtraum verwandelt haben.
+                            </p>
+
                         </div>
-                    </nav>
+                        <img src="bilder/1.jpg" alt="banner" class="banner-image">
+                    </div>
+
+                    <div class="slider-item">
+						<div class="banner-content">
+							<h2 class="banner-title">test</h2>
+							<p>
+                            Als Louisa eines der seltenen Stipendien für die Highclare Academy erhält, erhofft sie sich einen Neuanfang. Nicht nur, weil sie als Studentin automatisch zum elitären Kreis des Ruby Circles zählt und ihr jeder Wunsch von den Augen abgelesen wird. Sondern auch, weil sie glaubt, dort endlich sicher vor den Lügen zu sein, die ihr Leben in einen Albtraum verwandelt haben.
+                            </p>
+							<div class="btn-wrap">
+								<a href="#" class="btn btn-outline-accent btn-accent-arrow">Read More<i
+								        class="icon icon-ns-arrow-right"></i></a>
+							</div>
+						</div>
+						<img src="" alt="banner" class="banner-image">
+					</div>
+
                 </div>
             </div>
         </div>
-    </header>
 
-    
+    </section>
     
 </body>
 
