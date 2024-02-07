80949aa692c5000ac1e3adf70b3889cd64973242
As-Itachi <danijelritan6@gmail.com>
2024-02-07 13:43:39 +0100

    Warenkorb Icon hinzugefügt

--- navbar/navbar-profil.php
+++ navbar/navbar-profil.php
@@ -1,18 +1,10 @@
-<?php if(!isset($_SESSION['idBenutzer'])){
+<?php if (!isset($_SESSION['idBenutzer'])) {
     header("location: ../login.php?login-required=true");
 } ?>
 <nav class="navbar navbar-expand-sm navbar-light">
     <div class="container">
         <a class="navbar-brand" href="#">Knjižara</a>
-        <button
-            class="navbar-toggler d-lg-none"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavId"
-            aria-controls="collapsibleNavId"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
->
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
@@ -20,17 +12,17 @@
                 <li class="nav-item">
                     <a class="nav-link" href="../hauptseite.php" aria-current="page">Hauptseite</a>
                 </li>
-                <?php if(isset($_SESSION['email'])) { ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="../profil/">Benutzerprofil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../warenkorb/">Warenkorb</a>
-                </li>
-                <?php } else {?>
-                <li class="nav-item">
-                    <a class="nav-link" href="../login.php">Anmelden</a>
-                </li>
+                <?php if (isset($_SESSION['email'])) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../profil/">Benutzerprofil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../warenkorb/">Warenkorb</a>
+                    </li>
+                <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../login.php">Anmelden</a>
+                    </li>
                 <?php } ?>
             </ul>
             <a class="nav-link" href="../logout.php">Abmelden</a>
