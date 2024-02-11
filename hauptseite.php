98a56ffebf0893d665f98fa00452be3e8ab8b495
Danijel Ritan <danijelritan6@gmail.com>
2024-02-11 12:09:32 +0100

    Anzahl von Büchern hinzugefügt und löschen gefixt

--- hauptseite.php
+++ hauptseite.php
@@ -5,10 +5,12 @@ require_once('include/dbConnection.php');
 if (isset($_POST['add_to_cart'])) {
     $productId = $_POST['product_id'];
     $userId = $_SESSION['idBenutzer'];
+    $quantity = $_POST['menge'];
 
-    $stmt = $pdo->prepare("INSERT INTO warenkorb (idBenutzer, idBuecher) VALUES (:idBenutzer, :idBuecher)");
+    $stmt = $pdo->prepare("INSERT INTO warenkorb (idBenutzer, idBuecher, menge) VALUES (:idBenutzer, :idBuecher, :menge)");
     $stmt->bindParam(':idBenutzer', $userId);
     $stmt->bindParam(':idBuecher', $productId);
+    $stmt->bindParam(':menge', $quantity);
     $stmt->execute();
 
     header('Location: hauptseite.php');
@@ -33,8 +35,8 @@ include_once("navbar/navbar.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Knjižara - Template</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6C6RzynM0/CUGx8H2nA81rJR7aYnKagocF2jKuOXg9FfGeT8W1uQW7LGcL7q" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
 
 </head>
@@ -75,79 +77,55 @@ include_once("navbar/navbar.php");
 
                                             <form method="post">
                                                 <input type="hidden" name="product_id" value="<?php echo $book['idBuecher'] ?>">
-                                                <div class="buttons">
-                                                    <button type="submit" name="add_to_cart" class="add-to-cart">Warenkorb</button>
-                                                    <input type="number" name="menge" class="menge" min="1" max="10" value="1">
-                                                </div>
+                                                <div class="buttons"> <button type="submit" name="add_to_cart" class="add-to-cart">Warenkorb</button>
+                                                <input type="number" name="menge" class="menge" min="1" max="10" value="1"> </div>
                                             </form>
 
                                         </figure>
                                         <figcaption>
-                                            <h3><?php echo $book['titel'] ?></h3>
-                                            <span><?php echo $book['autor'] ?></span>
+                                            <h3><?php echo $book['titel'] ?></h3> <span><?php echo $book['autor'] ?></span>
                                             <div class="item-price"><?php echo $book['preis'] ?>€</div>
                                         </figcaption>
                                     </div>
-                                </div>
-                            <?php endforeach; ?>
+                                </div> <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-
-    <!-- Section for all books -->
+    </section> <!-- Section for all books -->
     <section id="popular-books" class="bookshelf py-5 my-5">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="col-md-12">
-
                         <div class="section-header align-center">
-                            <div class="title">
-                                <span></span>
-                            </div>
-
+                            <div class="title"> <span></span> </div>
                         </div>
                         <h2 class="section-title">Beliebte Bücher</h2>
                     </div>
                     <div class="tab-content">
                         <div id="all-genre" data-tab-content class="active">
-                            <div class="row">
-                                <?php foreach ($allBooks as $book) : ?>
-                                    <div class="col-md-3">
+                            <div class="row"> <?php foreach ($allBooks as $book) : ?> <div class="col-md-3">
                                         <div class="product-item">
-                                            <figure class="product-style">
-                                                <img src="bilder/<?php echo $book['idBuecher'] ?>.jpg" alt="book" class="product-item">
+                                            <figure class="product-style"> <img src="bilder/<?php echo $book['idBuecher'] ?>.jpg" alt="book" class="product-item">
 
-                                                <form method="post">
-                                                    <input type="hidden" name="product_id" value="<?php echo $book['idBuecher'] ?>">
-                                                    <div class="buttons">
-                                                        <button type="submit" name="add_to_cart" class="add-to-cart">Warenkorb</button>
-                                                        <input type="number" name="menge" class="menge" min="1" max="10" value="1">
-                                                    </div>
+                                                <form method="post"> <input type="hidden" name="product_id" value="<?php echo $book['idBuecher'] ?>">
+                                                    <div class="buttons"> <button type="submit" name="add_to_cart" class="add-to-cart">Warenkorb</button>
+                                                    <input type="number" name="menge" class="menge" min="1" max="10" value="1"> </div>
                                                 </form>
-
-
+                                                
                                             </figure>
                                             <figcaption>
-                                                <h3><?php echo $book['titel'] ?></h3>
-                                                <span><?php echo $book['autor'] ?></span>
+                                                <h3><?php echo $book['titel'] ?></h3> <span><?php echo $book['autor'] ?></span>
                                                 <div class="item-price"><?php echo $book['preis'] ?>€</div>
                                             </figcaption>
                                         </div>
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
+                                    </div> <?php endforeach; ?> </div>
                         </div>
                     </div>
                 </div>
             </div>
-    </section>
-
-    <?php include_once("footer/footer.php"); ?>
-
+    </section> <?php include_once("footer/footer.php"); ?>
 </body>
 
 </html>
--- warenkorb.php
+++ warenkorb.php
@@ -2,14 +2,18 @@
 session_start();
 require_once('include/dbConnection.php');
 
-
 if (isset($_POST['remove_from_cart'])) {
-    $productId = $_POST['product_id'];
+    $productId = $_POST['idBuecher'];
     $userId = $_SESSION['idBenutzer'];
     $stmt = $pdo->prepare("DELETE FROM warenkorb WHERE idBenutzer = :idBenutzer AND idBuecher = :idBuecher");
     $stmt->bindParam(':idBenutzer', $userId);
     $stmt->bindParam(':idBuecher', $productId);
     $stmt->execute();
+
+    // Aktualisiere die Warenkorb-Session
+    if (isset($_SESSION['warenkorb'][$productId])) {
+        unset($_SESSION['warenkorb'][$productId]);
+    }
 }
 
 $userId = $_SESSION['idBenutzer'];
@@ -18,17 +22,22 @@ $stmt->bindParam(':idBenutzer', $userId);
 $stmt->execute();
 $booksInCart = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
-try{
+try {
     $stmt = $pdo->prepare("SELECT * FROM buecher WHERE idBuecher IN (" . implode(',', array_fill(0, count($booksInCart), '?')) . ")");
     $stmt->execute($booksInCart);
     $cartBooks = $stmt->fetchAll();
-} catch(Exception $e){
-    echo $e->getMessage(); echo "<br>";
+
+    $quantities = $pdo->prepare("SELECT idBuecher, SUM(menge) as quantity FROM warenkorb WHERE idBenutzer = :idBenutzer GROUP BY idBuecher");
+    $quantities->bindParam(':idBenutzer', $userId);
+    $quantities->execute();
+    $quantities = $quantities->fetchAll(PDO::FETCH_KEY_PAIR);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    echo "<br>";
     echo $stmt->queryString;
 }
 
-
-include_once("navbar/navbar.php");
+include_once("./navbar/navbar.php");
 ?>
 
 <!DOCTYPE html>
@@ -38,7 +47,7 @@ include_once("navbar/navbar.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Knjižara - Template</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9Tneoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./css/warenkorb.css">
 
@@ -49,7 +58,7 @@ include_once("navbar/navbar.php");
     // Calculate the total price
     $totalPrice = 0;
     foreach ($cartBooks as $book) {
-        $totalPrice += $book['preis'];
+        $totalPrice += $book['preis'] * ($quantities[$book['idBuecher']] ?? 1);
     }
     ?>
 
@@ -59,30 +68,30 @@ include_once("navbar/navbar.php");
                 <th scope="col">Titel</th>
                 <th scope="col">Autor</th>
                 <th scope="col">Preis</th>
+                <th scope="col">Menge</th>
                 <th scope="col">Aktion</th>
             </tr>
         </thead>
-        <tbody>
-            <?php foreach ($cartBooks as $book) : ?>
-                <tr>
+        <tbody> <?php foreach ($cartBooks as $book) : ?> <tr>
                     <td><?php echo $book['titel'] ?></td>
                     <td><?php echo $book['autor'] ?></td>
                     <td><?php echo $book['preis'] ?>€</td>
+                    <td> <?php $quantity = ($quantities[$book['idBuecher']] ?? 1);
+                            echo $quantity; ?> </td>
                     <td>
-                        <form method="post">
-                            <input type="hidden" name="product_id" value="<?php echo $book['idBuecher'] ?>">
-                            <button type="submit" name="remove_from_cart" class="btn btn-danger">Aus Warenkorb entfernen</button>
-                            
-                        </form>
+                        <form method="post"> <input type="hidden" name="idBuecher" value="<?php echo $book['idBuecher'] ?>"> <button type="submit" name="remove_from_cart" class="btn btn-danger">Aus Warenkorb entfernen</button> </form>
                     </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
+                </tr> <?php endforeach; ?> </tbody>
     </table>
-
     <div class="total-price">
         <h3>Gesamtpreis: <?php echo $totalPrice ?>€</h3>
     </div>
+    <div class="checkout"> <button class="btn btn-primary">Zur Kasse gehen</button> </div>
+
+    <?php
+        include_once("./footer/footer.php");
+    ?>
+
 </body>
 
 </html>
