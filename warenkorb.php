b2930e1b17e5881518a5c64239ce9faa2a0d4ab3
Danijel Ritan <danijelritan6@gmail.com>
2024-02-19 15:56:22 +0100

    test warenkorb bestellen

--- warenkorb.php
+++ warenkorb.php
@@ -2,6 +2,19 @@
 session_start();
 require_once('include/dbConnection.php');
 
+
+$userFilled = true;
+$user = $_SESSION['benutzerInfo'] ?? [];
+
+if ($user) {
+    foreach ($user as $field) {
+        if (empty($field)) {
+            $userFilled = false;
+            break;
+        }
+    }
+}
+
 if (isset($_POST['remove_from_cart'])) {
     $productId = $_POST['idBuecher'];
     $userId = $_SESSION['idBenutzer'];
@@ -12,6 +25,19 @@ if (isset($_POST['remove_from_cart'])) {
 
     if (isset($_SESSION['warenkorb'][$productId])) {
         unset($_SESSION['warenkorb'][$productId]);
+    }
+} elseif (isset($_POST['checkout'])) {
+    $userId = $_SESSION['idBenutzer'];
+    $stmt = $pdo->prepare("SELECT * FROM benutzer WHERE idBenutzer = :idBenutzer");
+    $stmt->bindParam(':idBenutzer', $userId);
+    $stmt->execute();
+    $user = $stmt->fetch();
+
+    if ($user['name'] && $user['email'] && $user['passwort'] && $user['geburtstag'] && $user['wohnort'] && $user['plz'] && $user['nname']) {
+        unset($_SESSION['warenkorb']);
+        echo "Vielen Dank für Ihre Bestellung!";
+    } else {
+        echo "Bitte füllen Sie alle Benutzerinformationen aus, um die Bestellung abzuschließen.";
     }
 }
 
@@ -53,14 +79,13 @@ include_once("./navbar/navbar.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Knjižara - Template</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9Tneoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdeldeliver.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
         function removeFromCart(bookId) {
             if (confirm("Möchten Sie dieses Buch wirklich aus dem Warenkorb entfernen?")) {
                 const form = document.createElement('form');
                 form.method = 'post';
                 form.action = '';
-
                 const input = document.createElement('input');
                 input.type = 'hidden';
                 input.name = 'idBuecher';
@@ -88,41 +113,47 @@ include_once("./navbar/navbar.php");
     }
     ?>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Titel</th>
-                <th scope="col">Autor</th>
-                <th scope="col">Preis</th>
-                <th scope="col">Menge</th>
-                <th scope="col">Aktion</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($cartBooks as $book) : ?>
+    <form method="post" action="">
+        <table class="table">
+            <thead>
                 <tr>
-                    <td><?php echo htmlspecialchars($book['titel']); ?></td>
-                    <td><?php echo htmlspecialchars($book['autor']); ?></td>
-                    <td><?php echo htmlspecialchars($book['preis']); ?> €</td>
-                    <td>
-                        <?php $quantity = ($quantities[$book['idBuecher']] ?? 1);
-                        echo htmlspecialchars($quantity); ?>
-                    </td>
-                    <td>
-                        <form method="post" action="">
-                            <input type="hidden" name="idBuecher" value="<?php echo htmlspecialchars($book['idBuecher']); ?>">
-                            <button type="button" class="btn btn-danger" onclick="removeFromCart(<?php echo htmlspecialchars($book['idBuecher']); ?>)">Aus Warenkorb entfernen</button>
-                        </form>
-                    </td>
+                    <th scope="col">Titel</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Preis</th>
+                    <th scope="col">Menge</th>
+                    <th scope="col">Aktion</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($cartBooks as $book) : ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($book['titel']); ?></td>
+                        <td><?php echo htmlspecialchars($book['autor']); ?></td>
+                        <td><?php echo htmlspecialchars($book['preis']); ?> €</td>
+                        <td>
+                            <?php $quantity = ($quantities[$book['idBuecher']] ?? 1);
+                            echo htmlspecialchars($quantity); ?>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-danger" onclick="removeFromCart(<?php echo htmlspecialchars($book['idBuecher']); ?>)">Aus Warenkorb entfernen</button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </form>
     <div class="total-price">
         <h3>Gesamtpreis: <?php echo $totalPrice; ?> €</h3>
     </div>
+
     <div class="checkout">
-        <button class="btn btn-primary">Zur Kasse gehen</button>
+        <?php if ($userFilled) : ?>
+            <form method="post" action="">
+                <button class="btn btn-primary" name="checkout" value="true">Zur Kasse gehen</button>
+            </form>
+        <?php else : ?>
+            <p>Bitte füllen Sie alle Benutzerinformationen aus, um die Bestellung abzuschließen.</p>
+        <?php endif; ?>
     </div>
 
     <?php
@@ -135,18 +166,18 @@ include_once("./navbar/navbar.php");
                 const form = document.createElement('form');
                 form.method = 'post';
                 form.action = '';
-
                 const input = document.createElement('input');
                 input.type = 'hidden';
-                input.name = 'remove_from_cart';
-                input.value = 'true';
+                input.name = 'idBuecher';
+                input.value = bookId;
                 form.appendChild(input);
 
-                const bookInput = document.createElement('input');
-                bookInput.type = 'hidden';
-                bookInput.name = 'idBuecher';
-                bookInput.value = bookId;
-                form.appendChild(bookInput);
+                const button = document.createElement('button');
+                button.type = 'submit';
+                button.name = 'remove_from_cart';
+                button.value = 'true';
+                button.textContent = 'Entfernen';
+                form.appendChild(button);
 
                 document.body.appendChild(form);
                 form.submit();
