09b5245cf95b34a969fcf11b5c4500d02ae1c779
tycreed <tylerstranig2006@gmail.com>
2024-02-26 09:57:39 +0100

    vergrößert (navbar)

--- profil/kontodaten.php
+++ profil/kontodaten.php
@@ -11,9 +11,11 @@ require_once('../include/dbConnection.php');
     <title>Knjižara - Profil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
     <style>
         .nav-list {
             border-right: 1px solid lightgray;
+          
         }
 
         a {
@@ -25,6 +27,7 @@ require_once('../include/dbConnection.php');
             color: white;
         }
     </style>
+    
 </head>
 
 <body>
