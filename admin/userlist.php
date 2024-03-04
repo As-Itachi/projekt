836f2f3fd1d26296a0e56d2416f0b2fbd3c89188
tycreed <tylerstranig2006@gmail.com>
2024-03-04 11:07:34 +0100

    benutzerliste ausgeben

--- admin/userlist.php
+++ admin/userlist.php
@@ -1,9 +1,10 @@
-<?php 
-    include_once('../include/dbConnection.php');
+<?php
+include_once('../include/dbConnection.php');
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,22 +13,25 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <style>
-        .nav-list{
-            border-right : 1px solid lightgray;
-        }
-        a{
-            text-decoration: none;
-            color: black;
-        }
-        li.active a{
-            color: white;
-        }
-    </style>
+    .nav-list {
+        border-right: 1px solid lightgray;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    li.active a {
+        color: white;
+    }
+</style>
+
 <body>
     <?php include_once('../navbar/navbar-admin.php'); ?>
     <div class="p-5 h-100 d-flex align-items-center justify-content-center">
         <div class="container p-3 border">
-        <div class="row d-flex align-items-center">
+            <div class="row d-flex align-items-center">
                 <div class="col-sm-2 pt-5 pb-5 h-100 nav-list">
                     <ul class="list-group">
                         <li class="list-group-item">
@@ -42,10 +46,60 @@
                     </ul>
                 </div>
                 <div class="col-md-10 d-flex flex-column align-items-center">
-                   <?php #TODO: Userliste Hinzufügen, mit Lösch und Bearbeitungsoptionen [Tyler] ?>
+                <?php
+    try {
+        $statement = $pdo->prepare("SELECT * FROM benutzer");
+
+        $statement->execute();
+
+        echo "<table>";
+
+
+        if ($statement->rowCount() > 0) {
+            while ($zeile = $statement->fetch()) {
+                echo 
+                    "<tr>" ."<td>". "<th>Vorname: </th>". $zeile['name'] . "</td>" .
+                    "</tr>" .
+
+                    "<tr>" ."<td>". "<th>Nachname: </th>". $zeile['nname']. "</td>".
+                    "</tr>" .
+
+                    "<tr>" ."<td>". "<th>Email: </th>". $zeile['email']. "</td>".
+                    "</tr>" .
+
+                    "<tr>" ."<td>". "<th>Geburtstag: </th>". $zeile['geburtstag']. "</td>".
+                    "</tr>" .
+
+                    "<tr>" ."<td>". "<th>Wohnort: </th>". $zeile['wohnort']. "</td>".
+                    "</tr>" .
+
+                    "<tr>" ."<td>". "<th>PLZ: </th>". $zeile['plz']. "</td>".
+                    "</tr>" ;
+                    
+                
+            }
+        }
+        echo "</table>";
+        
+    } catch (PDOException $ex) {
+        die("Fehler beim Ausgeben der Daten von der Datenbank!");
+    }
+
+    echo "</div>";
+
+
+
+    ?>
                 </div>
             </div>
         </div>
     </div>
+
+
+
+    
+    
+
 </body>
+
 </html>
