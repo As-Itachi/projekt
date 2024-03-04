ef057bf2efe76e41f40d0cf42c712f12f84d478a
tycreed <tylerstranig2006@gmail.com>
2024-03-04 10:46:51 +0100

    bücher löschen und hinzufügen fertig

--- admin/booklist.php
+++ admin/booklist.php
@@ -1,9 +1,10 @@
-<?php 
-    include_once('../include/dbConnection.php');
+<?php
+include_once('../include/dbConnection.php');
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,33 +13,36 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <style>
-        .nav-list{
-            border-right : 1px solid lightgray;
-        }
-        a{
-            text-decoration: none;
-            color: black;
-        }
-        li.active a{
-            color: white;
-        }
-    </style>
+    .nav-list {
+        border-right: 1px solid lightgray;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    li.active a {
+        color: white;
+    }
+</style>
+
 <body>
 
     <?php include_once('../navbar/navbar-admin.php'); ?>
     <div class="p-5 h-100 d-flex align-items-center justify-content-center">
         <div class="container p-3 border">
-        <div class="row d-flex align-items-center">
+            <div class="row d-flex align-items-center">
                 <div class="col-sm-2 pt-5 pb-5 h-100 nav-list">
                     <ul class="list-group">
                         <li class="list-group-item">
                             <a href="./addbooks.php">Bücher hinzufügen</a>
                         </li>
                         <li class="list-group-item active">
-                            <a href="./booklist.php">Bücherliste</a> 
-                            
+                            <a href="./booklist.php">Bücherliste</a>
+
                         </li>
-                        
+
                         <li class="list-group-item">
                             <a href="./userlist.php">Benutzerliste</a>
                         </li>
@@ -50,56 +54,97 @@
                     echo "<div class ='loeschen'>";
 
 
+                    if (
+                        isset($_GET['idBuecher']) && isset($_GET['seitenanzahl']) && isset($_GET['erscheinungsjahr']) && isset($_GET['titel'])
+                        && isset($_GET['beschreibung']) && isset($_GET['preis']) && isset($_GET['genre']) && isset($_GET['autor'])
+                    ) {
+
+                        $id = $_GET['idBuecher'];
+                        $seitenzahl = $_GET['seitenanzahl'];
+                        $erscheinungsdatum = $_GET['erscheinungsjahr'];
+                        $titel = $_GET['titel'];
+                        $beschreibung = $_GET['beschreibung'];
+                        $preis = $_GET['preis'];
+                        $genre = $_GET['genre'];
+                        $autor = $_GET['autor'];
+                    }
+
+
+
+                    if (isset($_POST['loeschen'])) {
+
+
+                        try {
+
+
+
+                            $delete = $pdo->prepare("DELETE FROM buecher WHERE idBuecher = :idBuecher");
+
+                            $delete->bindParam(':idBuecher', $_POST['idBuecher']);
+
+                            $delete->execute();
+
+
+                            echo "Buch wurde gelöscht";
+                        } catch (PDOException $e) {
+                            die("Das Buch konnte nicht gelöscht werden!");
+                        }
+                    } elseif (isset($_POST['bestaetigen']) && $_POST['bestaetigen'] == 'Nein') {
+                        exit();
+                    }
+
                     try {
                         $statement = $pdo->prepare("SELECT * FROM buecher");
 
                         $statement->execute();
 
                         echo "<table>";
-                        
+
 
                         if ($statement->rowCount() > 0) {
                             while ($zeile = $statement->fetch()) {
-                                
-                                    echo "<tr style='font-size:20px'>" .
-                                        "<td style='color: purple'>" . $zeile['titel'] . "</td>" .
-                                        "</tr>" .
-                                        "<tr>" .
-                                    
-                                        "<tr>" .
-                                        "<th>Seitenanzahl: </th>" .
-                                        "<td>" . $zeile['seitenanzahl'] . "</td>" .
-                                        "</tr>" .
-                                        "<th>Erscheinungsjahr: </th>" .
-                                        "<td>" . $zeile['erscheinungsjahr'] . "</td>" .
-                                        "</tr>"
-                                        ."<td>". "<input type='submit' name='loeschen' value='löschen'> <br>" ."</td>" ;
-                                
-                                        
+                                echo "<tr style='font-size:20px'>" .
+                                    "<td style='color: purple'>" . $zeile['titel'] . "</td>" .
+                                    "</tr>" .
+                                    "<tr>" .
+                                    "<tr>" .
+                                    "<th>Seitenanzahl: </th>" .
+                                    "<td>" . $zeile['seitenanzahl'] . "</td>" .
+                                    "</tr>" .
+                                    "<th>Erscheinungsjahr: </th>" .
+                                    "<td>" . $zeile['erscheinungsjahr'] . "</td>" .
+                                    "</tr>" .
+                                    "<tr>" .
+                                    "<form method='POST'>" . //Formular zum löschen erstellt
+                                    "<input type='hidden' name='idBuecher' value='" . $zeile['idBuecher'] . "'>" .
+                                    "<td>" . "<input type='submit' name='loeschen' value='löschen'>" . "</td>" .
+                                    "</form>" .
+                                    "</tr>";
                             }
                         }
 
-                    
+
 
                         echo "</table>";
-
-                    
-
-
                     } catch (PDOException $ex) {
                         die("Fehler beim Ausgeben der Daten von der Datenbank!");
                     }
 
                     echo "</div>";
 
+
+
                     ?>
+
+                   
                 </div>
             </div>
         </div>
     </div>
 
-    
-   
+
+
 
 </body>
+
 </html>
